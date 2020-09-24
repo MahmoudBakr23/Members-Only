dc3980427a58d8ff3c4d3f19b78f558472f0156d
vmwhoami <vmwhoami@gmail.com>
2020-09-24 17:10:38 +0300

    change sign_up to register button

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :members, path: '', path_names:{sign_in: 'login', sign_up:'register'} ,:controllers => { registrations: 'registrations' }
   resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
 end
