cc29eefe5b1ed7899127c802b87a9b23c26d6364
Mahmoud Bakr <mbakr6821@gmail.com>
2020-09-25 03:15:47 +0200

    extract the logic out from the views and right into the posts_helper.rb file

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,25 @@
 module PostsHelper
+  def show_post?(post)
+    post.post
+  end
+
+  def member_check?(post)
+    post.member.name if member_signed_in?
+  end
+
+  def member_username_check?(post)
+    post.member.username if member_signed_in?
+  end
+
+  def show_edit?(post)
+    link_to 'Edit', edit_post_path(post) if member_signed_in?
+  end
+
+  def show_destroy?(post)
+    link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' } if member_signed_in?
+  end
+
+  def show_show?(post)
+    link_to 'Show', post if member_signed_in?
+  end
 end
